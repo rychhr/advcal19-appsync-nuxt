3c3bedee8394d9ef05a2eadbe1e260ee99aac2ae
Ryoichi Hara <786618+rychhr@users.noreply.github.com>
2019-12-08 22:16:50 +0900

    Add amplify plugin

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,7 +27,7 @@ export default {
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: ['@/plugins/iview'],
+  plugins: ['@/plugins/iview', '@/plugins/amplify'],
   /*
    ** Nuxt.js dev-modules
    */
